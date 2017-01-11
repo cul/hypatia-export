8479edb8b57fa9ca5e52e4daa0c037512c30c91c
Carla Galarza <cmg2228@columbia.edu>
2017-01-11 15:09:44 -0500

    Moving export field task into module. Working example

--- lib/hyacinth_export.rb
+++ lib/hyacinth_export.rb
@@ -2,11 +2,53 @@ require 'csv'
 require 'hyacinth_export/values'
 
 module HyacinthExport
-  def self.export; end
+  def self.export_fields(item_type_id) # Exports all fields for the same ItemType
+    type = ItemType.find(item_type_id) # Find a class of item (journal article, image, Oral History Object).
+    # item = Item.find(4066)
+    header_line = "id,item_id,element_id,parent_id,data,created_at,updated_at"
+    headers = header_line.split(',').collect { |hdr| hdr.to_sym }
+
+    CSV.open('tmp/data/values.csv','w') do |csv|
+      csv.add_row(header_line.split(","))
+      Item.where("id IN (?)", [2240,3063,3064]).limit(3).each do |item|
+        value_ids_by_element = Hash.new {|h,k| h[k] = []}
+        values_by_id = {}
+        root = nil
+        item.values.each do |value|
+          values_by_id[value.id] = value
+          value_ids_by_element[value.element_id] << value.id
+          if value.element_id == type.element_id
+            root = value
+          end
+          fields = headers.map { |hdr| value.send hdr }
+          csv.add_row(fields)
+        end
+
+        # Gets the relationships between the different values attached to this item.
+        #
+        # values_hierarchy = Hash.new {|hash,key| hash[key] = Hash.new &hash.default_proc }
+        # current = values_hierarchy[root.id]
+        # def children(values, map, parent_id)
+        #   values.select {|v| v.parent_id == parent_id }.each do |v|
+        #     children(values, map[v.id], v.id)
+        #   end
+        # end
+        # children(values, current, root.id)
+        # puts values_hierarchy.inspect
+
+        # Hyacinth format is _?[a-z_]+(-\d)? for element
+        # leading underscore is for reserved tag names
+        # -\d is for multiple values that need to be collected
+        # (:) to separate hierarchy
+        # .string_key for controlled values
+        # .uri and .value for URI values/labels
+        # nothing for plain old data
+      end
+    end
+  end
 
   # Exporting with cached values. Does not require a db connection.
-  # Exporting to a CSV.
-  def self.export_values
+  def self.export_values #export_records
     elements_to_codes = elements_to_codes
 
     items = {'2240' => 'ac:128939', '3063' => 'ac:130062', '3064' => 'ac:130066'}
--- lib/tasks/hyacinth.rake
+++ lib/tasks/hyacinth.rake
@@ -26,45 +26,8 @@ namespace :hyacinth do
     # end
     # puts elements_to_codes.inspect
     # puts elements_hierarchy.inspect
+    HyacinthExport.export_fields(41) #4066
 
-    type = ItemType.find(41)
-    # item = Item.find(4066)
-    header_line = "id,item_id,element_id,parent_id,data,created_at,updated_at"
-    headers = header_line.split(',').collect { |hdr| hdr.to_sym }
-    open('tmp/data/values.csv','w') do |out|
-      out.print(header_line)
-      Item.where(:conditions => ["id IN (?)" , [2240,3063,3064]], :limit => 3).each do |item|
-        value_ids_by_element = Hash.new {|h,k| h[k] = []}
-        values_by_id = {}
-        root = nil
-        item.values.each do |value|
-          values_by_id[value.id] = value
-          value_ids_by_element[value.element_id] << value.id
-          if value.element_id == type.element_id
-            root = value
-          end
-          fields = headers.map { |hdr| item.send hdr }
-          out.print CSV.generate_line(fields)
-        end
-        values_hierarchy = Hash.new {|hash,key| hash[key] = Hash.new &hash.default_proc }
-        current = values_hierarchy[root.id]
-        def children(values, map, parent_id)
-          values.select {|v| v.parent_id == parent_id }.each do |v|
-            children(values, map[v.id], v.id)
-          end
-        end
-        children(values, current, root.id)
-        puts values_hierarchy.inspect
-
-        # Hyacinth format is _?[a-z_]+(-\d)? for element
-        # leading underscore is for reserved tag names
-        # -\d is for multiple values that need to be collected
-        # (:) to separate hierarchy
-        # .string_key for controlled values
-        # .uri and .value for URI values/labels
-        # nothing for plain old data
-      end
-    end
   end
 
   task :export_values => :environment do
